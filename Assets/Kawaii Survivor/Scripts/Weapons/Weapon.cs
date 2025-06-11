cfe1708144a97da6a03e3308f76d84c535bae168
NThuanit <nbthuan.icloud@gmail.com>
2025-06-11 08:45:48 +0700

    Weapon attack

--- Assets/Kawaii Survivor/Scripts/Weapons/Weapon.cs
+++ Assets/Kawaii Survivor/Scripts/Weapons/Weapon.cs
@@ -1,10 +1,32 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    enum State
+    {
+        Idle,
+        Attack
+    }
+
+    private State state;
+
+    [Header("Elements")]
+    [SerializeField] private Transform hitDetectionTransform;
+    [SerializeField] private float hitDetectionRadius;
+
     [Header("Settings")]
     [SerializeField] private float range;
     [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attack")]
+    [SerializeField] private int damage;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float attackDelay;
+    private float attackTimer;
+
+    private List<Enemy> damagedEnemies = new List<Enemy>(); 
 
     [Header("Animations")]
     [SerializeField] private float aimLerp;
@@ -13,13 +35,22 @@ public class Weapon : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        state = State.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        AutoAim();
+        switch (state)
+        {
+            case State.Idle:
+                AutoAim();
+                break;
+
+            case State.Attack:
+                Attacking();
+                break;
+        }
 
     }
 
@@ -29,12 +60,77 @@ public class Weapon : MonoBehaviour
 
         Vector2 targetUpVector = Vector3.up;
 
-        if (closestEnemy != null )
+        if (closestEnemy != null)
+        {
+            ManageAttack();
             targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
+        }
 
         transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
 
+        IncrementAttackTimer();
     }
+
+    private void ManageAttack()
+    {
+        attackTimer += Time.deltaTime;
+
+        if (attackTimer >= attackDelay)
+        {
+            attackTimer = 0;
+            StartAttack();
+        }
+    }
+
+    private void IncrementAttackTimer()
+    {
+        attackTimer += Time.deltaTime;
+    }
+
+    [NaughtyAttributes.Button]
+    private void StartAttack()
+    {
+        animator.Play("Attack");
+        state = State.Attack;
+
+        damagedEnemies.Clear();
+    }
+    private void Attacking()
+    {
+        Attack();
+    }
+
+    private void StopAttack()
+    {
+        state = State.Idle;
+        //clear the attacked enemies
+        //damaged enemies list
+        damagedEnemies.Clear();
+    }
+
+
+    private void Attack()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(hitDetectionTransform.position, hitDetectionRadius, enemyMask);
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            Enemy enemy = enemies[i].GetComponent<Enemy>();
+
+            //1. is the enemy inside of the list?
+            if (!damagedEnemies.Contains(enemy))
+            {
+                enemy.TakeDamage(damage);
+                damagedEnemies.Add(enemy);
+            }
+
+            //2. if no let's attack, add it to the list
+
+            //3. if yes, let's continue, check the next enemy
+
+
+        }
+    }
+
     private Enemy GetClosestEnemy()
     {
         Enemy closestEnemy = null;
@@ -67,5 +163,8 @@ public class Weapon : MonoBehaviour
     {
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(transform.position, range);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(hitDetectionTransform.position, hitDetectionRadius);
     }
 }
