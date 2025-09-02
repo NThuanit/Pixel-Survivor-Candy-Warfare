07236a859c2c9abfed15bf473b5e517f6c4e0422
NThuanit <nbthuan.icloud@gmail.com>
2025-09-02 22:41:27 +0700

    bug -.-

---
+++ Assets/Kawaii Survivor/Scripts/Effects/BumpyButton.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class BumpyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    [Header("Elements")]
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!button.interactable)
+            return;
+
+        LeanTween.cancel(button.gameObject);
+        LeanTween.scale(gameObject,  new Vector2(1.1f, 0.9f), 0.6f)
+            .setEase(LeanTweenType.easeOutElastic)
+            .setIgnoreTimeScale(true); 
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!button.interactable)
+            return;
+
+        LeanTween.cancel(button.gameObject);
+        LeanTween.scale(gameObject, Vector2.one, 0.6f)
+            .setEase(LeanTweenType.easeOutElastic)
+            .setIgnoreTimeScale(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!button.interactable)
+            return;
+
+        LeanTween.cancel(button.gameObject);
+        LeanTween.scale(gameObject, Vector2.one, 0.6f)
+            .setEase(LeanTweenType.easeOutElastic)
+            .setIgnoreTimeScale(true);
+    }
+}
